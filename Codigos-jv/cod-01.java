39b52cbe0929ffbd363701eefd509ba5200d909a
Ticiane Barros <110422127+Ticianebarros@users.noreply.github.com>
2023-03-22 15:19:32 -0300

    Novo

---
+++ Codigos-jv/cod-01.java
@@ -0,0 +1,21 @@
+class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
+
+import java.util.Scanner;
+class sum
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+    }
+}
+
+
